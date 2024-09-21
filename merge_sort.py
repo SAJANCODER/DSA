ac330a074af9410306a492760341a59fdefc886d
SAJAN D <sajanneyveli@gmail.com>
2024-09-21 22:58:27 +0530

    Add existing file

--- merge_sort.py
+++ merge_sort.py
@@ -1,15 +1,20 @@
-def merge_array(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
+#merge sort
 
+def merge_sort(arr):
+    if len(arr)>1:
+        mid=len(arr)//2
+    #splitting the array
         left_array=arr[:mid]
         right_array=arr[mid:]
-        merge_array(left_array)
-        merge_array(right_array)
 
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        #merging the two half
         i=j=k=0
-        while i<len(left_array) and j< len(right_array):
-            if left_array[i]< right_array[j]:
+
+        while i<len(left_array) and j<len(right_array):
+            if left_array[i]<right_array[j]:
                 arr[k]=left_array[i]
                 i+=1
             else:
@@ -21,17 +26,18 @@ def merge_array(arr):
             arr[k]=left_array[i]
             i+=1
             k+=1
+
         while j<len(right_array):
             arr[k]=right_array[j]
             j+=1
             k+=1
-
-
-n=int(input("enter the length os the array:"))
+    
+                
+n=int(input("enter the array length:"))
 arr=[]
 for i in range(n):
     num=int(input(f"enter the {i} element:"))
     arr.append(num)
-print("orginal array",arr)
-merge_array(arr)
+print("orginal _array",arr)
+merge_sort(arr)
 print("merged_array",arr)
