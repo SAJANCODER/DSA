7f5fed48331089f6373a88dfc10a3a2af304e08c
SAJAN D <sajanneyveli@gmail.com>
2024-09-21 23:52:12 +0530

    try

--- insertation_sort.py
+++ insertation_sort.py
@@ -1,13 +1,9 @@
 def insertion_sort(arr):
-    # Start from the second element (index 1)
     for i in range(1, len(arr)):
-        # The current element to be inserted into the sorted part
-        current_value = arr[i]
-        j=i-1
-        while j>=0 and arr[j]> current_value:
-            arr[j+1]=arr[j]
+        j=i
+        while arr[j-1]>arr[j] and j>0:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
             j-=1
-        arr[j+1]=current_value
 
 # Example usage
 arr = [12, 11, 13, 5, 6]
--- try.py
+++ try.py
@@ -1,37 +1,11 @@
-def merge_array(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
+def insert_sort(arr):
+    for i in range(1,len(arr)):
+        j=i
+        while arr[j-1]>arr[j] and j>0:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+            j-=1
 
-        left_array=arr[:mid]
-        right_array=arr[mid:]
-        merge_array(left_array)
-        merge_array(right_array)
-
-        i=j=k=0
-        while i<len(left_array) and j< len(right_array):
-            if left_array[i]< right_array[j]:
-                arr[k]=left_array[i]
-                i+=1
-            else:
-                arr[k]=right_array[j]
-                j+=1
-            k+=1
-
-        while i<len(left_array):
-            arr[k]=left_array[i]
-            i+=1
-            k+=1
-        while j<len(right_array):
-            arr[k]=right_array[j]
-            j+=1
-            k+=1
-
-
-n=int(input("enter the length os the array:"))
-arr=[]
-for i in range(n):
-    num=int(input(f"enter the {i} element:"))
-    arr.append(num)
-print("orginal array",arr)
-merge_array(arr)
-print("merged_array",arr)
+arr=[12,4,5,1,0,8,5,4,3]
+print(arr)
+insert_sort(arr)
+print(arr)
