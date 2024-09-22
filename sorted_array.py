fa8263290c60ba554e4685054aa4e01025564a04
SAJAN D <sajanneyveli@gmail.com>
2024-09-22 22:12:12 +0530

    rewrite

--- sorted_array.py
+++ sorted_array.py
@@ -1,37 +1,38 @@
-###sorted array
-##
-##def SelectionSort(array,n):
-##    for i in range(n):
-##        Min=i
-##        for j in range(i+1,n):
-##            if(array[j]<array[Min]):
-##                Min=j
-##        array[i],array[Min]=array[Min],array[i]
-##    return array
-##n=int(input("enter the length of array:"))
-##array=[]
-##i=0
-##for i in range(n):
-##    print(f"enter the element {i}:")
-##    array_elements=int(input())
-##    array.append(array_elements)
-##print("orginal array",array)
-##print("Sorted array",SelectionSort(array,n))
 
-def Selection_sort(array,n):
-    for i in range(n):
-        Min=i
-        for j in range(i+1,n):
-            if (array[j]<array[Min]):
-                Min=j
-        array[i],array[Min]=array[Min],array[i]
-    return array
-n=int(input("\n Enter the Length of array:\n"))
-array=[]
-i=0
-for i in range(n):
-    array_elements=int(input(f"Enter The Elements Of the Array{i}:"))
-    array.append(array_elements)
-print("\nOrginal_Array\n",array)
-print("\n Sorted_Array\n",Selection_sort(array,n))
-    
+#
+# def Selection_sort(array,n):
+#     for i in range(n):
+#         Min=i
+#         for j in range(i+1,n):
+#             if (array[j]<array[Min]):
+#                 Min=j
+#         array[i],array[Min]=array[Min],array[i]
+#     return array
+# n=int(input("\n Enter the Length of array:\n"))
+# array=[]
+# i=0
+# for i in range(n):
+#     array_elements=int(input(f"Enter The Elements Of the Array{i}:"))
+#     array.append(array_elements)
+# print("\nOrginal_Array\n",array)
+# print("\n Sorted_Array\n",Selection_sort(array,n))
+#
+
+# selection sort
+
+
+def selection_sort(arr):
+    if len(arr) > 1:
+        for i in range(1, len(arr)):
+            Min = i
+            for j in range(i):
+                if arr[j] > arr[Min]:
+                    Min = j
+                    arr[i], arr[Min] = arr[Min], arr[i]
+        return arr
+
+
+arr = [13, 0, 45, 99, 1, 12, 10]
+print(arr)
+selection_sort(arr)
+print("after sort", arr)
--- try.py
+++ try.py
@@ -1,3 +1,18 @@
-a=[3,1,5,8,9,2]
-for i in range(len(a)-1,0,-1):
-    print(i)
+#selection sort
+
+
+def selection_sort(arr):
+    if len(arr)>1:
+        for i in range(1,len(arr)):
+            Min=i
+            for j in range(i):
+                if arr[j]>arr[Min]:
+                    Min=j
+                    arr[i],arr[Min]=arr[Min],arr[i]
+        return arr
+                       
+    
+arr=[13,0,45,99,1,12,10]
+print(arr)
+selection_sort(arr)
+print("after sort",arr)
